c21165b1cf6dbe20af62f4bd1238f6beef71006a
DevDanielAlbano <victor98moura@gmail.com>
2025-04-07 19:57:38 -0300

    update project in admin and template

--- outflows/admin.py
+++ outflows/admin.py
@@ -1,31 +1,10 @@
-from django.urls import reverse_lazy
-from django.views.generic import ListView, CreateView, DetailView
-from . import models, forms
+from django.contrib import admin
+from . import models
 
 
-class OutflowListView(ListView):
-    model = models.Outflow
-    template_name = 'outflow_list.html'
-    context_object_name = 'outflows'
-    paginate_by = 4
-
-    def get_queryset(self):
-        queryset = super().get_queryset()
-        product = self.request.GET.get('product')
-
-        if product:
-            queryset = queryset.filter(product__title__icontains=product)
-
-        return queryset
+class OutflowAdmin(admin.ModelAdmin):
+    list_display = ('product', 'quantity', 'created_at', 'update_at',)
+    search_fields = ('product__title',)
 
 
-class OutflowCreateView(CreateView):
-    model = models.Outflow
-    template_name = 'outflow_create.html'
-    form_class = forms.OutflowForm
-    success_url = reverse_lazy('outflow_list')
-
-
-class OutflowDetailView(DetailView):
-    model = models.Outflow
-    template_name = 'outflow_detail.html'
+admin.site.register(models.Outflow, OutflowAdmin)
